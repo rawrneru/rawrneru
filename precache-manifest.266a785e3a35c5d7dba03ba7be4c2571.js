1457d1f77a3ebf7549ac2814521c8e6ab4c6bee7
Alexander Mitch Sugiharto <32443784+rawrneru@users.noreply.github.com>
2020-08-14 23:09:03 +0000

    Updates

---
+++ precache-manifest.266a785e3a35c5d7dba03ba7be4c2571.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcabb173e9db42591353d21cfef0bc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18531a9d43ee548f7749",
+    "url": "/static/css/main.7540feb5.chunk.css"
+  },
+  {
+    "revision": "e5f27d209f789df6c134",
+    "url": "/static/js/2.42615cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42615cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18531a9d43ee548f7749",
+    "url": "/static/js/main.e573a4be.chunk.js"
+  },
+  {
+    "revision": "73d151299925abc6a7ce",
+    "url": "/static/js/runtime-main.770a3a53.js"
+  },
+  {
+    "revision": "4aaf0e5fef8acb576a3a83a365b2910a",
+    "url": "/static/media/profile-pic.4aaf0e5f.png"
+  }
+]);
